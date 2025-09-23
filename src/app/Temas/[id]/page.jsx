a06babe5a82806e0cd804eacb76a30723a755a8e
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-09-23 14:55:01 -0300

    feat: continuation of [id] theme

--- src/app/Temas/[id]/page.jsx
+++ src/app/Temas/[id]/page.jsx
@@ -2,9 +2,10 @@
 
 import { use, useEffect, useState } from "react";
 import styles from "./[id].module.css";
+import Link from "next/link";
 
 export default function TemaPage({ params }) {
-    const [id , setId] = useState(null);
+    const [id, setId] = useState(null);
     const [theme, setTheme] = useState(null);
 
     useEffect(() => {
@@ -30,15 +31,42 @@ export default function TemaPage({ params }) {
 
     return (
         <div className={styles.container}>
-            <div className={styles.titleArea}>
-                <h1 className={styles.title}>Detalhes do Tema</h1>
+            <div className={styles.card}>
                 {theme && (
                     <div>
-                        <h2>{theme.name}</h2>
-                        <p>{theme.description}</p>
-                        <p><strong>Categoria:</strong> {theme.category}</p>
-                        <p><strong>Duração:</strong> {theme.duration}</p>
-                        <p><strong>Nível:</strong> {theme.level}</p>
+                        <div className={styles.level}>
+                            {theme.level && (
+                                <span className={`${styles.temaNivel} ${theme.level?.toLowerCase() === 'iniciante' ? styles.nivelIniciante :
+                                        (theme.level?.toLowerCase() === 'intermediario' || theme.level?.toLowerCase() === 'intermediário') ? styles.nivelIntermediario :
+                                            (theme.level?.toLowerCase() === 'avancado') ? styles.nivelAvancado : ''
+                                    }`}>
+                                    {theme.level}
+                                </span>
+                            )}
+                        </div>
+                        <div className={styles.headerCard}>
+                            <img
+                                src={`/images/${theme.photo}`}
+                                alt="icone do tema selecionado"
+                                className={styles.themeImage}
+                            />
+                            <h1 className={styles.themeName}>{theme.name}</h1>
+                        </div>
+                        <div className={styles.line}></div>
+                        <div className={styles.smallInfo}>
+                            <p className={styles.category}>{theme.category}</p>
+                            <p className={styles.duration}>{theme.duration}</p>
+                        </div>
+
+                        <div className={styles.main}>
+                            <p className={styles.description}>{theme.description}</p>
+                            <button 
+                                className={styles.startButton}
+                                onClick={() => window.location.href = `/Temas/${theme.id}/Aulas`}
+                            >
+                                Começar
+                            </button>
+                        </div>
                     </div>
                 )}
             </div>
