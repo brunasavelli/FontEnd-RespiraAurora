3bec6ba121781b17a1d86331732f5d8e2e48020c
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-09-23 16:18:29 -0300

    feat: start 'SobreMim' page

--- src/app/SobreMim/page.jsx
+++ src/app/SobreMim/page.jsx
@@ -1,12 +1,21 @@
-"use client"
+"use client";
 
-import React from "react";
+import { useState, useEffect } from "react";
 import styles from "./SobreMim.module.css";
+import Link from "next/link";
 
 export default function SobreMim() {
     return (
         <div className={styles.container}>
-            <h1>Sobre Mim Page</h1>
+            <div className={styles.card}>
+                <div className={styles.headerCard}>
+                    <img src="/images/bruna.webp" alt="Foto de Bruna Savelli" className={styles.photo} />
+                    <div className={styles.headerText}>
+                        <h1 className={styles.name}>Bruna Savelli</h1>
+                        <p>Desenvolvedora e criadora de RespiraAurora</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
